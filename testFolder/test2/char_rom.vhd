50c2000855dbe2fb0e08c9e4784a7745dec9b11c
I-Gringeri <95489135+I-Gringeri@users.noreply.github.com>
2025-05-06 13:26:49 -0400

    Update char_rom.vhd

--- testFolder/test2/char_rom.vhd
+++ testFolder/test2/char_rom.vhd
@@ -1,3 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity char_rom is
+    port (
+        clk     : in  std_logic;
+        char    : in  std_logic_vector(7 downto 0);
+        row     : in  std_logic_vector(2 downto 0);
+        pixels  : out std_logic_vector(7 downto 0)
+    );
+end char_rom;
+
 architecture Behavioral of char_rom is
     type rom_type is array(0 to 127, 0 to 7) of std_logic_vector(7 downto 0);
     signal rom : rom_type := (
@@ -60,4 +73,11 @@ architecture Behavioral of char_rom is
 
         others => (others => x"00")
     );
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            pixels <= rom(to_integer(unsigned(char)))(to_integer(unsigned(row)));
+        end if;
+    end process;
 end Behavioral;
