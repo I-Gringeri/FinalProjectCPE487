710d1fa1c6e15edfc71ff1179a21ad007da5aca4
William Hines <whines@stevens.edu>
2025-05-10 18:23:01 -0400

    rename twinkle fsm to lyrics fsm

--- testFolder/test2/lyrics_fsm.vhd
+++ testFolder/test2/lyrics_fsm.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity twinkle_fsm is
+entity lyrics_fsm is
     port (
         clk         : in std_logic;
         reset       : in std_logic;
