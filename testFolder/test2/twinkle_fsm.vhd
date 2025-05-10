312972e42929f15757e54cc717649381c0ea934d
William Hines <whines@stevens.edu>
2025-05-10 18:20:27 -0400

    Incorporated play signal and added line number output

--- testFolder/test2/twinkle_fsm.vhd
+++ testFolder/test2/twinkle_fsm.vhd
@@ -6,17 +6,19 @@ entity twinkle_fsm is
     port (
         clk         : in std_logic;
         reset       : in std_logic;
-        line_index  : out integer range 0 to 7
+        play        : in std_logic;
+        line_index  : out integer range 0 to 7;
+        current_line : out integer rand 0 to 7 :=0
     );
 end twinkle_fsm;
 
 architecture Behavioral of twinkle_fsm is
     signal counter      : integer := 0;
-    signal current_line : integer range 0 to 7 := 0;
     constant MAX_COUNT  : integer := 400_000_000; -- Change line every ~4s at 100MHz
 begin
     process(clk)
     begin
+        if play = '1' then
         if rising_edge(clk) then
             if reset = '1' then
                 counter      <= 0;
@@ -25,7 +27,7 @@ begin
                 if counter >= MAX_COUNT then
                     counter <= 0;
                     if current_line = 7 then
-                        current_line <= 0;
+                        play <= '0';
                     else
                         current_line <= current_line + 1;
                     end if;
@@ -33,6 +35,7 @@ begin
                     counter <= counter + 1;
                 end if;
             end if;
+        end if;
         end if;
     end process;
 
